3eec53006219962d33b20cc17f1f1718c623106d
GoharRamzan <goharnaqshbandi@gmail.com>
2025-05-01 17:15:15 +0500

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 const projects = [
-  
+
   {
     title: 'Project One',
     description: 'A description of the project goes here.',
