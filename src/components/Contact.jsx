7ac60db6196e0f15ec97eb7736d83a6b0b268dac
GoharRamzan <goharnaqshbandi@gmail.com>
2025-05-09 17:21:27 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,7 +25,6 @@ const Contact = () => {
             transition={{ duration: 0.6 }}
         >
 
-            
             <div className="container mx-auto">
                 <h2 className="text-4xl md:text-5xl font-bold text-center mb-12 text-teal-400">Contact Me</h2>
                 <div className="flex justify-center">
